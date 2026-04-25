2ab50b341af12f4faa3abf3e439a4edbd5d7ff95
Prakashkanar <prakashkanar.2236@gmail.com>
2026-04-25 19:47:50 +0530

    model path and pickle path change

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -9,8 +9,8 @@ class predictPipeline:
 
     def predict(self, feature):
         try:
-            model_path = 'artifacts\model.pkl'
-            preprocessor_path = 'artifacts\preprocessor.pkl'
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path = 'artifacts/preprocessor.pkl'
             model = load_object(file_path = model_path)
             preprocessor = load_object(file_path=preprocessor_path)
             data_scaled = preprocessor.transform(feature)
