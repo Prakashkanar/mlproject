6065b739918d09c04ec840aeb3b96e2cfcb9df74
Prakashkanar <prakashkanar.2236@gmail.com>
2026-04-25 21:13:35 +0530

    reading_score and writing_score fix. result = Error is error occur

--- app.py
+++ app.py
@@ -15,25 +15,28 @@ def index():
 
 @app.route('/predictdata', methods = ['GET', 'POST'])
 def predict_datapoint():
-    if request.method == 'GET':
-        return render_template('home.html')
-    else:
-        data = CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-        )
+    try:
+        if request.method == 'GET':
+            return render_template('home.html')
+        else:
+            data = CustomData(
+                gender=request.form.get('gender'),
+                race_ethnicity=request.form.get('ethnicity'),
+                parental_level_of_education=request.form.get('parental_level_of_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                reading_score=float(request.form.get('reading_score')),
+                writing_score=float(request.form.get('writing_score'))
+            )
 
-        pred_df = data.get_data_as_data_frame()
-        print(pred_df)
+            pred_df = data.get_data_as_data_frame()
+            print(pred_df)
 
-        predict_pipeline = predictPipeline()
-        results = predict_pipeline.predict(pred_df)
-        return render_template('home.html', results = results[0])
+            predict_pipeline = predictPipeline()
+            results = predict_pipeline.predict(pred_df)
+            return render_template('home.html', results = round(results[0], 2))
+    except Exception as e:
+        raise render_template('home.html', results=f"Error: {str(e)}")
     
 
 if __name__ == '__main__':
